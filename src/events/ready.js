0807bc43e4f97980009ec5ffde36585c7e0c4f80
Wrenchi <70622477+WrenchWD@users.noreply.github.com>
2022-03-17 18:18:17 +0530

    Added Dnd Status

--- src/events/ready.js
+++ src/events/ready.js
@@ -6,7 +6,7 @@ const Wrenchi = require("../lib/Wrenchi");
 module.exports = async (client) => {
     (client.Ready = true),
     client.log("Successfully Logged in as " + client.user.tag);
-    client.user.setPresence({ activities: [{ name: `To Wrench's Code`, type: "WATCHING" }] });
+    client.user.setPresence({ activities: [{ name: `To Wrench's Code`, type: "LISTENING" }], status: "dnd" });
 
     // Erela.js init stuffs
     client.Manager.init(client.user.id);
