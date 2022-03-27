40a38798281b01a8749e9bf0d2f19a4df5ed8c98
Wrenchi <70622477+WrenchWD@users.noreply.github.com>
2022-03-27 18:48:53 +0530

    Changed Status

--- src/events/ready.js
+++ src/events/ready.js
@@ -5,7 +5,7 @@ const Wrenchi = require("../lib/Wrenchi");
 */
 module.exports = async (client) => {
     console.log("Successfully Logged in as " + client.user.tag);
-    client.user.setPresence({ activities: [{ name: `To Wrench's Code`, type: "LISTENING" }], status: "dnd" });
+    client.user.setPresence({ activities: [{ name: `Wrench HideOut`, type: "WATCHING" }], status: "dnd" });
 
     // Erela.js init stuffs
     client.Manager.init(client.user.id);
