5bbaa3c8bef2a506bee3815f6a9da2752868ca18
Wrenchi <70622477+WrenchWD@users.noreply.github.com>
2022-03-28 21:02:27 +0530

    Updated Command: Ban (Permission checking)

--- src/commands/slash/ban.js
+++ src/commands/slash/ban.js
@@ -7,21 +7,31 @@ const command = new SlashCommand()
     .addMentionableOption((option) => option.setName("user").setDescription("Whom I need to destroy sir?").setRequired(true))
     .addStringOption((option) => option.setName("reason").setDescription("Why do I need to ban him?").setRequired(true))
     .setRun(async (client, interaction, options) => {
-        const mention = interaction.options.getMentionable('user');
-        const reason = interaction.options.getString('reason');
-        if (!mention) return;
-        if (!reason) return;
+        if (interaction.member.permissions.has("BAN_MEMBERS")) {
+            const mention = interaction.options.getMentionable('user');
+            const reason = interaction.options.getString('reason');
+            if (!mention) return;
+            if (!reason) return;
 
-        const member = interaction.guild.members.cache.get(mention.id);
-        if (!member) return interaction.reply({ content: "No user found!" })
+            const member = interaction.guild.members.cache.get(mention.id);
+            if (!member) return interaction.reply({ content: "No user found!" })
 
-        if (member.bannable) {
-            member.ban({
-                reason: reason
-            });
-            interaction.reply({ content: `${member.user.tag} has been banned for ${reason}` });
+            if (member.bannable) {
+                member.ban({
+                    reason: reason
+                });
+
+                member.createDM(true).then(async (dm) => {
+                    await dm.send({
+                        content: `You have been banned from ${interaction.guild.name} for ${reason}`
+                    });
+                })
+                interaction.reply({ content: `${member.user.tag} has been banned for ${reason}` });
+            } else {
+                interaction.reply({ content: `${member.user.tag} cannot be banned.` });
+            }
         } else {
-            interaction.reply({ content: `${member.user.tag} cannot be banned.` });
+            interaction.reply({ content: `Please die without permissions!` });
         }
     });
 
