f915d981bf20a2ecbf0c21e96792b13fef1a5b7e
Wrenchi <70622477+WrenchWD@users.noreply.github.com>
2022-07-12 18:48:06 +0530

    idk ;-;

--- src/commands/legacy/sticket.ts
+++ src/commands/legacy/sticket.ts
@@ -8,27 +8,54 @@ const Command = {
     },
 
     run: async function (client: Wrenchi, message: Message, args: string[]) {
+        const openTicketCategory = message.guild.channels.cache.find(channel => channel.name === "Opened Tickets");
+        if (!openTicketCategory) {
+            const msg = await message.channel.send("No Opened Tickets category found, making category.....");
+
+            setTimeout(() => {
+                message.guild.channels.create("Opened Tickets", {
+                    type: "GUILD_CATEGORY",
+                    position: 0
+                });
+
+                if (msg.deletable) msg.delete();
+            }, 5000);
+        }
+
+        const closeTicketCategory = message.guild.channels.cache.find(channel => channel.name === "Closed Tickets");
+        if (!closeTicketCategory) {
+            const msg = await message.channel.send("No Closed Tickets category found, making category.....");
+
+            setTimeout(() => {
+                message.guild.channels.create("Closed Tickets", {
+                    type: "GUILD_CATEGORY"
+                });
+
+                if (msg.deletable) msg.delete();
+            }, 5000);
+        }
+
         const ticketButton = new MessageActionRow()
-        .addComponents(
-            new MessageButton()
-                .setCustomId("ticket_open")
-                .setLabel("🎫")
-                .setStyle("SUCCESS")
-        );
+            .addComponents(
+                new MessageButton()
+                    .setCustomId("ticket_open")
+                    .setLabel("🎫")
+                    .setStyle("SUCCESS")
+            );
 
         const embed = new MessageEmbed()
-        .setAuthor({ name: "Wrenchi Ticket System v1.0", iconURL: client.user.displayAvatarURL({ dynamic: true }) })
-        .addFields([
-            {
-                name: "How to open a ticket?",
-                value: "Click the 🎫 button to open a ticket"
-            },
-            {
-                name: "About this system",
-                value: "The current ticket system is in the beta version now, so don't except so much."
-            }
-        ])
-        .setFooter({ text: "Proudly made by Wrench#0012", iconURL: client.user.displayAvatarURL() });
+            .setAuthor({ name: "Wrenchi Ticket System v1.0", iconURL: client.user.displayAvatarURL({ dynamic: true }) })
+            .addFields([
+                {
+                    name: "How to open a ticket?",
+                    value: "Click the 🎫 button to open a ticket"
+                },
+                {
+                    name: "About this system",
+                    value: "The current ticket system is in the beta version now, so don't except so much."
+                }
+            ])
+            .setFooter({ text: "Proudly made by Wrench#0012", iconURL: client.user.displayAvatarURL() });
         await message.channel.send({ embeds: [embed], components: [ticketButton] });
     },
 };
--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,23 +1,30 @@
 import Wrenchi from "../lib/Wrenchi"
-import { 
-    CommandInteractionOptionResolver, 
-    Interaction, 
-    MessageActionRow, 
-    MessageButton, 
-    MessageEmbed, 
+import {
+    CommandInteractionOptionResolver,
+    Interaction,
+    MessageActionRow,
+    MessageButton,
+    MessageEmbed,
 } from "discord.js"
 
+interface ITickets {
+    guildID: string,
+    channelID: string,
+    username: string,
+    userID: string,
+    state: string,
+    createdAt: Date,
+    deletedAt: Date
+}
+
 export default async (client: Wrenchi, interaction: Interaction) => {
-    if (interaction.channel.type === "DM") return
+    const mysql = await client.mysql;
 
     if (interaction.isCommand()) {
         let command = client.SlashCommands.find(
             (x) => x.name == interaction.commandName
         );
-        if (!command || !command.run)
-            return interaction.reply(
-                "Sorry the command you used doesn't have any run function"
-            );
+
         command.run(client, interaction, interaction.options as CommandInteractionOptionResolver);
     } else if (interaction.isContextMenu()) {
         let command = client.ContextCommands.find((x) => x.name === interaction.commandName);
@@ -26,46 +33,59 @@ export default async (client: Wrenchi, interaction: Interaction) => {
         command.run(client, interaction);
     } else if (interaction.isButton()) {
         if (interaction.customId === "ticket_open") {
-            const ticketChannel = await interaction.guild.channels.create(`ticket-${interaction.user.username}`, {
-                permissionOverwrites: [
-                    {
-                        allow: ["SEND_MESSAGES", "VIEW_CHANNEL", "ADD_REACTIONS", "ATTACH_FILES"],
-                        id: interaction.user.id
-                    },
-                    {
-                        deny: ["VIEW_CHANNEL"],
-                        id: interaction.guild.id
-                    }
-                ]
-            });
+            const getTickets = "SELECT * FROM tickets WHERE userID = ?"
+            const query = mysql.query(getTickets, [interaction.user.id]);
+            await interaction.reply({ content: "Getting ready to open a ticket...." });
+            // await interaction.deferReply({ ephemeral: true });
 
-            interaction.reply({ content: `Ticket has been created: <#${ticketChannel.id}>`, ephemeral: true });
-            if (ticketChannel.type === "GUILD_TEXT") {
-                const closeButton = new MessageButton()
-                .setCustomId("ticket_close")
-                .setLabel("🔒")
-                .setStyle("DANGER")
+            query.on("result", async (res: any, index) => {
+                if (res.userID === interaction.user.id) {
+                    await interaction.editReply({ content: "You already have an open ticket" });
+                    return
+                }
 
-                const embed = new MessageEmbed()
-                .setAuthor({ name: `Hello ${interaction.user.username}` })
-                .addFields([
-                    {
-                        name: "How to close the ticket?",
-                        value: "Click 🔒 to close the ticket"
-                    }
-                ])
-                .setFooter({ text: `Ticket opened at ${new Date().toLocaleString()}` });
+                await interaction.deferReply({ ephemeral: true, fetchReply: true });
 
-                await ticketChannel.send({ embeds: [embed], components: [new MessageActionRow().addComponents(closeButton)] });
-                const mysql = await client.mysql
-                const query = "INSERT INTO tickets (guildID, channelID, username, userID, state, createdAt) VALUES (?)"
-                const values = [interaction.guild.id, ticketChannel.id, interaction.user.username, interaction.user.id, "Opened", new Date()]
-
-                mysql.query(query, [values], (err, result: any) => {
-                    if (err) throw err;
-                    console.log("Number of records inserted: " + result.affectedRows);
+                const ticketChannel = await interaction.guild.channels.create(`ticket-${interaction.user.username}`, {
+                    permissionOverwrites: [
+                        {
+                            allow: ["SEND_MESSAGES", "VIEW_CHANNEL", "ADD_REACTIONS", "ATTACH_FILES"],
+                            id: interaction.user.id
+                        },
+                        {
+                            deny: ["VIEW_CHANNEL"],
+                            id: interaction.guild.id
+                        }
+                    ]
                 });
-            }
+
+                await interaction.followUp({ content: `Ticket has been created: <#${ticketChannel.id}>`, ephemeral: true, fetchReply: true });
+                if (ticketChannel.type === "GUILD_TEXT") {
+                    const closeButton = new MessageButton()
+                        .setCustomId("ticket_close")
+                        .setLabel("🔒")
+                        .setStyle("DANGER")
+
+                    const embed = new MessageEmbed()
+                        .setAuthor({ name: `Hello ${interaction.user.username}` })
+                        .addFields([
+                            {
+                                name: "How to close the ticket?",
+                                value: "Click 🔒 to close the ticket"
+                            }
+                        ])
+                        .setFooter({ text: `Ticket opened at ${new Date().toLocaleString()}` });
+
+                    await ticketChannel.send({ embeds: [embed], components: [new MessageActionRow().addComponents(closeButton)] });
+                    const query = "INSERT INTO tickets (guildID, channelID, username, userID, state, createdAt) VALUES (?)"
+                    const values = [interaction.guild.id, ticketChannel.id, interaction.user.username, interaction.user.id, "Opened", new Date()]
+
+                    mysql.query(query, [values], (err, result: any) => {
+                        if (err) throw err;
+                        console.log("Number of records inserted: " + result.affectedRows);
+                    });
+                }
+            });
         }
     }
 }
